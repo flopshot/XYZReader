7e0f38bcf096dab546c3d1c6d32b89b6ba13a0d1
flopshot <n112358@gmail.com>
2017-03-22 12:40:22 -0700

    Initial Commit
    -Using new data source for img files: see Config.java
    -Update app:build.gradle

---
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -0,0 +1,21 @@
+package com.example.xyzreader.remote;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Config {
+    public static final URL BASE_URL;
+    // INITIAL COMMIT
+    // USING HACK URL TO POINT TO GITHUB REPO OF CORRECT DATA. ORIGINAL LINK https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json
+    // IS SHOWING 404 ERROR. PLEASE SEE UDACITY FORUM DISCUSSION FOR MORE INFO https://discussions.udacity.com/t/links-to-dropbox-for-make-your-app-material-not-working/227857
+    static {
+        URL url = null;
+        try {
+            url = new URL("https://raw.githubusercontent.com/Protino/dump/master/Lego/data.json" );
+        } catch (MalformedURLException ignored) {
+            // TODO: throw a real error
+        }
+
+        BASE_URL = url;
+    }
+}
